0c2ada61fe01db8a6741281c943358cefa7ef6f5
wijaya <muhammadwijaya881@gmail.com>
2025-02-08 10:50:46 +0700

    menambah kan file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba dari git bash ke github</title>
+</head>
+<body>
+    <h2>up skill</h2>
+</body>
+</html>
