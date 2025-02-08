b8085f076929f6e0b1e8bc642b3c53c03539554a
wijaya <muhammadwijaya881@gmail.com>
2025-02-08 10:51:59 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h2>up skill</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque impedit eaque amet eum, quia perferendis. Ex, totam aspernatur! Quam asperiores aspernatur consequuntur tenetur cupiditate ab, error non cum dolores dolor.</p>
 </body>
 </html>
